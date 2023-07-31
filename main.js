59def474ecf552af637619b1e79c727db3c8a4cb
Myriam Bibas <pattybibas@gmail.com>
2023-07-31 16:35:59 -0300

    verificacion de edad del usuario

---
+++ main.js
@@ -0,0 +1,8 @@
+let edadIngresada = confirm ("Hola!, tenes mas de 18 años?")
+if (edadIngresada===true){
+    console.log("Podes comprar tranquil@");
+
+}else {
+    console.log("No podes comprar eres menor de 18")
+
+}
