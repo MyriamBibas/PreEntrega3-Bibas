7ce98e133e6fcfe1be24f13eb1b07dd2376231c3
Myriam Bibas <pattybibas@gmail.com>
2023-07-31 17:29:10 -0300

    aplicacion del login

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-let edadIngresada = confirm ("Hola!, tenes mas de 18 años?")
-if (edadIngresada===true){
-    console.log("Podes comprar tranquil@");
+let edadIngresada = confirm("Hola!, tenes mas de 18 años?")
+
+if (edadIngresada === true){
+    console.log ("Podes comprar tranquil@");
 
 }else {
-    console.log("No podes comprar eres menor de 18")
+    console.log ("No podes comprar, eres menor de 18");
+}
 
+// login de la pagina 
+let login = true
+while (login===true){
+let usuario = prompt ("Ingresa tu Usuario");
+let contraseña = prompt ( "Ingresa tu contraseña");
+let validar = (usuario=== "Myri" && contraseña === "1234b");
+
+if (validar){
+    alert ('Bienvenido$(usuario)');
+    login=false
+    breack
+    alert ("Usuario o contraseña incorrecto");
+}
 }
