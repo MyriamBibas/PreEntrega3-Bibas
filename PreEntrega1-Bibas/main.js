8d892dc4350cffe28ad04599b83af180eaca5856
Myriam Bibas <pattybibas@gmail.com>
2023-08-31 13:15:44 -0300

    se agregaron imagenes al index

--- PreEntrega1-Bibas/main.js
+++ PreEntrega1-Bibas/main.js
@@ -12,34 +12,29 @@ if (edadIngresada === true) {
     console.log("No podes comprar, eres menor de 18");
 }
 
-// login de la pagina 
-let login = true;
-
-while (login) {
-    let usuario = prompt("Ingresa tu Usuario");
-    let contraseña = prompt("Ingresa tu contraseña");
-    let validar = usuario === "myri" && contraseña === "1234b";
-
-    if (validar) {
-        alert(`Bienvenido ${usuario}`);
-        break;
-    } else {
-        alert("Usuario o contraseña incorrecto");
-    }
-}
-
-
 /*lista de productos*/
 let productos = [
     { id: 1, nombre: "whiskey", precio: 2500 },
-    { id: 2, nombre: "vocka", precio: 3000 },
-    { id: 3, nombre: "vino tinto", precio: 5000 },
-    { id: 4, nombre: "vino blanco", precio: 2300 },
-    { id: 5, nombre: "ron", precio: 6000 },
+    { id: 2, nombre: "vocka absolub", precio: 3000 },
+    { id: 3, nombre: "vino tinto mauro", precio: 5000 },
+    { id: 4, nombre: "vino blanco da vinci", precio: 2300 },
+    { id: 5, nombre: "ron havanna", precio: 6000 },
     { id: 6, nombre: "fernet", precio: 2800 },
     { id: 7, nombre: "champagne", precio: 1000 },
-    { id: 8, nombre: "cerveza", precio: 1500 },
+    { id: 8, nombre: "cerveza heineken", precio: 1500 },
     { id: 9, nombre: "anis", precio: 3800 },
+    { id: 10, nombre: "vocka smirnoff", precio: 3900 },
+    { id: 11, nombre: "vino tinto alma mora", precio:3500},
+    { id: 12, nombre: "vino tinto trapiche cabernet sauvignon", precio: 4000},
+    { id: 13, nombre: "vino blanco  trapiche cosecha tardia ", precio: 3900 },
+    { id: 14, nombre: "vino blanco alma mora pinot grigio", precio: 4000 },
+    { id: 15, nombre: "ron santa teresa", precio: 7000 },
+    { id: 16, nombre: "ron cacique", precio: 6800 },
+    { id: 17, nombre: "ron carupano añejo", precio: 7500 },
+    { id: 18, nombre: "cerveza corona", precio: 1000},
+    { id: 19, nombre: "cerveza solera", precio: 1500 },
+    { id: 20, nombre: "cerveza polar", precio: 1800},
+
 ];
 
 /*suma de impuestos a los productos*/
