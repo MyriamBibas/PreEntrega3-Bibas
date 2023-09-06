17e10845c7f0c6eca92a3e9608df9efbe6819de9
Myriam Bibas <pattybibas@gmail.com>
2023-09-06 12:42:48 -0300

    correccion de imagens y base de datos para js

--- PreEntrega1-Bibas/main.js
+++ PreEntrega1-Bibas/main.js
@@ -1,83 +1,76 @@
-function saludar() {
-    console.log("Hola")
-}
-saludar()
 
-let edadIngresada = confirm("¿tenes mas de 18 años?")
-
-if (edadIngresada === true) {
-    console.log("Podes comprar tranquil@");
-
-} else {
-    console.log("No podes comprar, eres menor de 18");
-}
+/* permiso para entrar*/
+Swal.fire({
+    title: 'Hey, tenes mas de 18 años?',
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: 'Si',
+    denyButtonText: `No`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire('Podes comprar tranquil@', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('No podes comprar, deciles a tus papis', '', 'info')
+    }
+  })
+  /* Barra de navegacion*/
+ 
+let searchForm = document.getElementById("search-form");
+let navbarWidth = document.querySelector(".navbar-collapse").offsetWidth;
+let searchFormWidth = searchForm.offsetWidth;
+let marginLeft = (navbarWidth - searchFormWidth) / 2;
+searchForm.style.marginLeft = marginLeft + "px";
 
 /*lista de productos*/
-let productos = [
-    { id: 1, nombre: "whiskey", precio: 2500 },
-    { id: 2, nombre: "vocka absolub", precio: 3000 },
-    { id: 3, nombre: "vino tinto mauro", precio: 5000 },
-    { id: 4, nombre: "vino blanco da vinci", precio: 2300 },
-    { id: 5, nombre: "ron havanna", precio: 6000 },
-    { id: 6, nombre: "fernet", precio: 2800 },
-    { id: 7, nombre: "champagne", precio: 1000 },
-    { id: 8, nombre: "cerveza heineken", precio: 1500 },
-    { id: 9, nombre: "anis", precio: 3800 },
-    { id: 10, nombre: "vocka smirnoff", precio: 3900 },
-    { id: 11, nombre: "vino tinto alma mora", precio: 3500 },
-    { id: 12, nombre: "vino tinto trapiche cabernet sauvignon", precio: 4000 },
-    { id: 13, nombre: "vino blanco  trapiche cosecha tardia ", precio: 3900 },
-    { id: 14, nombre: "vino blanco alma mora pinot grigio", precio: 4000 },
-    { id: 15, nombre: "ron santa teresa", precio: 7000 },
-    { id: 16, nombre: "ron cacique", precio: 6800 },
-    { id: 17, nombre: "ron carupano añejo", precio: 7500 },
-    { id: 18, nombre: "cerveza corona", precio: 1000 },
-    { id: 19, nombre: "cerveza solera", precio: 1500 },
-    { id: 20, nombre: "cerveza polar", precio: 1800 },
+function principal (){
 
+  let productoStock = [
+    { id: 1, nombre: "whiskey", precio: 2500, imagen: "imagenes-proyecto/whiskey.jpg" },
+    { id: 2, nombre: "vocka absolub", precio: 3000, imagen: "imagenes-proyecto/vocka.jpg" },
+    { id: 3, nombre: "vino tinto mauro", precio: 5000, imagen: "imagenes-proyecto/vino-tinto.jpg" },
+    { id: 4, nombre: "vino blanco da vinci", precio: 2300, imagen: "imagenes-proyecto/vino-blanco.jpg" },
+    { id: 5, nombre: "ron havanna", precio: 6000, imagen: "imagenes-proyecto/ron.jpg" },
+    { id: 6, nombre: "fernet", precio: 2800, imagen: "imagenes-proyecto/fernet.jpg" },
+    { id: 7, nombre: "champagne", precio: 1000, imagen: "imagenes-proyecto/champagne.jpg" },
+    { id: 8, nombre: "cerveza heineken", precio: 1500, imagen: "imagenes-proyecto/cerveza.png" },
+    { id: 9, nombre: "anis", precio: 3800, imagen: "imagenes-proyecto/anis.jpg" },
+    { id: 10, nombre: "vocka smirnoff", precio: 3900, imagen: "imagenes-proyecto/vockasmirnoff.jpg" },
+    { id: 11, nombre: "vino tinto alma mora", precio: 3500, imagen: "imagenes-proyectovinotintoalmamora.jpg" }, // Corregí el nombre de la imagen
+    { id: 12, nombre: "vino tinto trapiche cabernet sauvignon", precio: 4000, imagen: "imagenes-proyecto/vinotintotrapiche.jpg" }, // Corregí el nombre de la imagen
+    { id: 13, nombre: "vino blanco trapiche cosecha tardía", precio: 3900, imagen: "imagenes-proyecto/vinoblancotrapiche.jpg" }, // Corregí el nombre de la imagen
+    { id: 14, nombre: "ron santa teresa", precio: 7000, imagen: "imagenes-proyecto/ronsantateresa.jpg" }, // Corregí el nombre de la imagen
+    { id: 15, nombre: "ron cacique", precio: 6800, imagen: "imagenes-proyecto/roncacique.jpg" }, // Corregí el nombre de la imagen
+    { id: 16, nombre: "ron carupano añejo", precio: 7500, imagen: "imagenes-proyecto/roncarupano.jpg" }, // Corregí el nombre de la imagen
+    { id: 17, nombre: "cerveza corona", precio: 1000, imagen: "imagenes-proyecto/cervezacorona.jpg" }, // Corregí el nombre de la imagen
+    { id: 18, nombre: "cerveza solera", precio: 1500, imagen: "imagenes-proyecto/cervezapolar.jpg" }, // Corregí el nombre de la imagen
+    { id: 29, nombre: "cerveza polar", precio: 1800, imagen: "imagenes-proyecto/cervezapolar.jpg" } // Corregí el nombre de la imagen
 ];
 
-/*suma de impuestos a los productos*/
-function sumarImpuestos(productos) {
-    const impuestoPorcentaje = 0.21;
-    return productos.map(producto => ({
-        ...producto,
-        precioConImpuestos: producto.precio * (1 + impuestoPorcentaje)
-    }));
-}
-/*mostrar lista de producto*/
-function mostrarProductosConImpuestos(productosMostrados) {
-    let mensaje = 'Lista de Productos con Impuestos incluidos:\n\n';
+let contenedor = document.getElementById('producto');
 
-    productosMostrados.forEach(producto => {
-        mensaje += `${producto.nombre} - $${producto.precioConImpuestos.toFixed(2)}\n`;
-    });
-
-    alert(mensaje);
-}
-/*filtrar los productos por nombre*/
-function filtrarProductosPorNombre() {
-    const filtroNombre = prompt('Ingresa el nombre del producto que buscas:');
-    const productosFiltrados = productos.filter(producto =>
-        producto.nombre.toLowerCase().includes(filtroNombre.toLowerCase())
-    );
-
-    if (productosFiltrados.length > 0) {
-        const productosConImpuestos = sumarImpuestos(productosFiltrados);
-        mostrarProductosConImpuestos(productosConImpuestos);
-    } else {
-        alert('No se encontraron productos con ese nombre.');
-    }
+productoStock.forEach(function (producto) {
+    renderizarTarjeta(producto);
+});
 }
 
-/*aparece en la pantalla*/
-window.onload = () => {
-    filtrarProductosPorNombre();
-    const productosConImpuestos = sumarImpuestos(productos);
+function renderizarTarjeta(producto) {
+let tarjetaProducto = document.createElement("div");
+tarjetaProducto.classList.add("tarjetaProducto");
+tarjetaProducto.innerHTML = `
+    <h3>${producto.nombre}</h3>
+    <p>${producto.precio}</p>
+    <img src="${producto.imagen}" alt="${producto.nombre}"> 
+`;
 
-};
+contenedor.appendChild(tarjetaProducto);
+}
+
+principal();
+
+
 /*footer*/
+
 document.getElementById('ir-arriba').addEventListener('click', function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+})
 
