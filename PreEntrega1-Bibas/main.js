820a375f8d43124def419c8bd29ef2093d929a92
Myriam Bibas <pattybibas@gmail.com>
2023-09-06 16:10:47 -0300

    arreglo de botones

--- PreEntrega1-Bibas/main.js
+++ PreEntrega1-Bibas/main.js
@@ -13,60 +13,106 @@ Swal.fire({
       Swal.fire('No podes comprar, deciles a tus papis', '', 'info')
     }
   })
-  /* Barra de navegacion*/
- 
-let searchForm = document.getElementById("search-form");
-let navbarWidth = document.querySelector(".navbar-collapse").offsetWidth;
-let searchFormWidth = searchForm.offsetWidth;
-let marginLeft = (navbarWidth - searchFormWidth) / 2;
-searchForm.style.marginLeft = marginLeft + "px";
-
+  
 /*lista de productos*/
+principal();
 function principal (){
 
   let productoStock = [
-    { id: 1, nombre: "whiskey", precio: 2500, imagen: "imagenes-proyecto/whiskey.jpg" },
-    { id: 2, nombre: "vocka absolub", precio: 3000, imagen: "imagenes-proyecto/vocka.jpg" },
-    { id: 3, nombre: "vino tinto mauro", precio: 5000, imagen: "imagenes-proyecto/vino-tinto.jpg" },
-    { id: 4, nombre: "vino blanco da vinci", precio: 2300, imagen: "imagenes-proyecto/vino-blanco.jpg" },
-    { id: 5, nombre: "ron havanna", precio: 6000, imagen: "imagenes-proyecto/ron.jpg" },
-    { id: 6, nombre: "fernet", precio: 2800, imagen: "imagenes-proyecto/fernet.jpg" },
-    { id: 7, nombre: "champagne", precio: 1000, imagen: "imagenes-proyecto/champagne.jpg" },
-    { id: 8, nombre: "cerveza heineken", precio: 1500, imagen: "imagenes-proyecto/cerveza.png" },
-    { id: 9, nombre: "anis", precio: 3800, imagen: "imagenes-proyecto/anis.jpg" },
-    { id: 10, nombre: "vocka smirnoff", precio: 3900, imagen: "imagenes-proyecto/vockasmirnoff.jpg" },
-    { id: 11, nombre: "vino tinto alma mora", precio: 3500, imagen: "imagenes-proyectovinotintoalmamora.jpg" }, // Corregí el nombre de la imagen
-    { id: 12, nombre: "vino tinto trapiche cabernet sauvignon", precio: 4000, imagen: "imagenes-proyecto/vinotintotrapiche.jpg" }, // Corregí el nombre de la imagen
-    { id: 13, nombre: "vino blanco trapiche cosecha tardía", precio: 3900, imagen: "imagenes-proyecto/vinoblancotrapiche.jpg" }, // Corregí el nombre de la imagen
-    { id: 14, nombre: "ron santa teresa", precio: 7000, imagen: "imagenes-proyecto/ronsantateresa.jpg" }, // Corregí el nombre de la imagen
-    { id: 15, nombre: "ron cacique", precio: 6800, imagen: "imagenes-proyecto/roncacique.jpg" }, // Corregí el nombre de la imagen
-    { id: 16, nombre: "ron carupano añejo", precio: 7500, imagen: "imagenes-proyecto/roncarupano.jpg" }, // Corregí el nombre de la imagen
-    { id: 17, nombre: "cerveza corona", precio: 1000, imagen: "imagenes-proyecto/cervezacorona.jpg" }, // Corregí el nombre de la imagen
-    { id: 18, nombre: "cerveza solera", precio: 1500, imagen: "imagenes-proyecto/cervezapolar.jpg" }, // Corregí el nombre de la imagen
-    { id: 29, nombre: "cerveza polar", precio: 1800, imagen: "imagenes-proyecto/cervezapolar.jpg" } // Corregí el nombre de la imagen
+    { id: 1, nombre: "Whiskey", precio: 2500, imagen: "imagenes-proyecto/whiskey.jpg" },
+    { id: 2, nombre: "Vocka absolub", precio: 3000, imagen: "imagenes-proyecto/vocka.jpg" },
+    { id: 3, nombre: "Vino Tinto Mauro", precio: 5000, imagen: "imagenes-proyecto/vino-tinto.jpg" },
+    { id: 4, nombre: "Vino Blanco Da Vinci", precio: 2300, imagen: "imagenes-proyecto/vino-blanco.jpg" },
+    { id: 5, nombre: "Ron Havanna", precio: 6000, imagen: "imagenes-proyecto/ron.jpg" },
+    { id: 6, nombre: "Fernet", precio: 2800, imagen: "imagenes-proyecto/fernet.jpg" },
+    { id: 7, nombre: "Champagne", precio: 1000, imagen: "imagenes-proyecto/champagne.jpg" },
+    { id: 8, nombre: "Cerveza Heineken", precio: 1500, imagen: "imagenes-proyecto/cerveza.png" },
+    { id: 9, nombre: "Anis", precio: 3800, imagen: "imagenes-proyecto/anis.jpg" },
+    { id: 10, nombre: "Vocka Smirnoff", precio: 3900, imagen: "imagenes-proyecto/vockasmirnoff.jpg" },
+    { id: 11, nombre: "Vino Tinto Alma Mora", precio: 3500, imagen: "imagenes-proyectovinotintoalmamora.jpg" }, // Corregí el nombre de la imagen
+    { id: 12, nombre: "Vino Tinto Trapiche Cabernet Sauvignon", precio: 4000, imagen: "imagenes-proyecto/vinotintotrapiche.jpg" }, // Corregí el nombre de la imagen
+    { id: 13, nombre: "Vino Blanco Trapiche Cosecha Tardía", precio: 3900, imagen: "imagenes-proyecto/vinoblancotrapiche.jpg" }, // Corregí el nombre de la imagen
+    { id: 14, nombre: "Ron Santa Teresa", precio: 7000, imagen: "imagenes-proyecto/ronsantateresa.jpg" }, // Corregí el nombre de la imagen
+    { id: 15, nombre: "Ron Cacique", precio: 6800, imagen: "imagenes-proyecto/roncacique.jpg" }, // Corregí el nombre de la imagen
+    { id: 16, nombre: "Ron Carupano Añejo", precio: 7500, imagen: "imagenes-proyecto/roncarupano.jpg" }, // Corregí el nombre de la imagen
+    { id: 17, nombre: "Cerveza Corona", precio: 1000, imagen: "imagenes-proyecto/cervezacorona.jpg" }, // Corregí el nombre de la imagen
+    { id: 18, nombre: "Cerveza Solera", precio: 1500, imagen: "imagenes-proyecto/cervezapolar.jpg" }, // Corregí el nombre de la imagen
+    { id: 29, nombre: "Cerveza Polar", precio: 1800, imagen: "imagenes-proyecto/cervezapolar.jpg" } // Corregí el nombre de la imagen
 ];
+/* Barra de navegacion*/
+let inputBuscador = document.getElementById('buscador')
+let botonBuscar = document.getElementById('buscar')
+botonBuscar.addEventListener('click', () => filtro (productoStock, inputBuscador, "nombre"))
 
-let contenedor = document.getElementById('producto');
+let botonFinalizarCompra = document.getElementById("finalizarCompra")
+  botonFinalizarCompra.addEventListener("click", finalizarCompra)
 
-productoStock.forEach(function (producto) {
-    renderizarTarjeta(producto);
-});
+  renderizarCarrito()
+  renderizarTarjetas(productoStock)
+
+}
+/*carrito */
+function finalizarCompra() {
+  let carrito = recuperarCarrito()
+  if (carrito.length > 0) {
+    localStorage.removeItem("carrito")
+    renderizarCarrito()
+    alert("Muchas gracias por su compra, te esperamos pronto")
+  } else {
+    alert("Agrega productos en tu carrito para poder continuar")
+  }
+}
+function filtrar(productos, input,
+   propiedad) {
+  let productosFiltrados = productos.filter(producto => producto[propiedad])
+renderizarTarjetas(productosFiltrados)
+}
+function renderizarTarjetas(productos) {
+  let contenedor = document.getElementById("productos")
+  contenedor.innerHTML = ""
+  productos.forEach(producto => {
+    let tarjetaProducto = document.createElement("div")
+    tarjetaProducto.classList.add("tarjetaProducto")
+    tarjetaProducto.innerHTML = `
+      <h3>${producto.nombre}</h3>
+      <p>${producto.precio}</p>
+      <button id=${producto.id}>Agregar al carrito</button>
+    `
+    contenedor.appendChild(tarjetaProducto)
+    let botonAgregarAlCarrito = document.getElementById(producto.id)
+    botonAgregarAlCarrito.addEventListener("click", (e) => agregarAlCarrito(productos, e))
+    
+  })
+  }
+  function agregarAlCarrito(productos, e) {
+    let carrito = recuperarCarrito()
+    let productoBuscado = productos.find(producto => producto.id === Number(e.target.id))
+    let productoEnCarrito = carrito.find(producto => producto.id === productoBuscado.id)
+
+localStorage.setItem("carrito", JSON.stringify(carrito))
+
+  renderizarCarrito()
+}
+function renderizarCarrito() {
+  let contenedor = document.getElementById("carrito")
+  contenedor.innerHTML = ""
+  let carrito = recuperarCarrito()
+
+  carrito.forEach(producto => {
+    let tarjetaProducto = document.createElement("div")
+    tarjetaProducto.innerHTML = `
+      <p>${producto.nombre}</p>
+      <p>${producto.precioUnitario}</p>
+      <p>${producto.unidades}</p>
+      <p>${producto.subtotal}</p>
+    `
+    contenedor.appendChild(tarjetaProducto)
+  })
 }
 
-function renderizarTarjeta(producto) {
-let tarjetaProducto = document.createElement("div");
-tarjetaProducto.classList.add("tarjetaProducto");
-tarjetaProducto.innerHTML = `
-    <h3>${producto.nombre}</h3>
-    <p>${producto.precio}</p>
-    <img src="${producto.imagen}" alt="${producto.nombre}"> 
-`;
-
-contenedor.appendChild(tarjetaProducto);
-}
-
-principal();
-
+function recuperarCarrito() {
+  return localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : []
+  }
 
 /*footer*/
 
