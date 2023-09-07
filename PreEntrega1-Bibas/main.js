505b3c07b3afbe1cd19c99fa3997e311823ee6ac
Myriam Bibas <pattybibas@gmail.com>
2023-09-06 23:06:31 -0300

    modificacion del carrito

--- PreEntrega1-Bibas/main.js
+++ PreEntrega1-Bibas/main.js
@@ -1,22 +1,22 @@
 
 /* permiso para entrar*/
 Swal.fire({
-    title: 'Hey, tenes mas de 18 años?',
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: 'Si',
-    denyButtonText: `No`,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire('Podes comprar tranquil@', '', 'success')
-    } else if (result.isDenied) {
-      Swal.fire('No podes comprar, deciles a tus papis', '', 'info')
-    }
-  })
-  
+  title: 'Hey, tenes mas de 18 años?',
+  showDenyButton: true,
+  showCancelButton: true,
+  confirmButtonText: 'Si',
+  denyButtonText: `No`,
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire('Podes comprar tranquil@', '', 'success')
+  } else if (result.isDenied) {
+    Swal.fire('No podes comprar, deciles a tus papis', '', 'info')
+  }
+})
+
 /*lista de productos*/
 principal();
-function principal (){
+function principal() {
 
   let productoStock = [
     { id: 1, nombre: "Whiskey", precio: 2500, imagen: "imagenes-proyecto/whiskey.jpg" },
@@ -29,94 +29,131 @@ function principal (){
     { id: 8, nombre: "Cerveza Heineken", precio: 1500, imagen: "imagenes-proyecto/cerveza.png" },
     { id: 9, nombre: "Anis", precio: 3800, imagen: "imagenes-proyecto/anis.jpg" },
     { id: 10, nombre: "Vocka Smirnoff", precio: 3900, imagen: "imagenes-proyecto/vockasmirnoff.jpg" },
-    { id: 11, nombre: "Vino Tinto Alma Mora", precio: 3500, imagen: "imagenes-proyectovinotintoalmamora.jpg" }, // Corregí el nombre de la imagen
-    { id: 12, nombre: "Vino Tinto Trapiche Cabernet Sauvignon", precio: 4000, imagen: "imagenes-proyecto/vinotintotrapiche.jpg" }, // Corregí el nombre de la imagen
-    { id: 13, nombre: "Vino Blanco Trapiche Cosecha Tardía", precio: 3900, imagen: "imagenes-proyecto/vinoblancotrapiche.jpg" }, // Corregí el nombre de la imagen
-    { id: 14, nombre: "Ron Santa Teresa", precio: 7000, imagen: "imagenes-proyecto/ronsantateresa.jpg" }, // Corregí el nombre de la imagen
-    { id: 15, nombre: "Ron Cacique", precio: 6800, imagen: "imagenes-proyecto/roncacique.jpg" }, // Corregí el nombre de la imagen
-    { id: 16, nombre: "Ron Carupano Añejo", precio: 7500, imagen: "imagenes-proyecto/roncarupano.jpg" }, // Corregí el nombre de la imagen
-    { id: 17, nombre: "Cerveza Corona", precio: 1000, imagen: "imagenes-proyecto/cervezacorona.jpg" }, // Corregí el nombre de la imagen
-    { id: 18, nombre: "Cerveza Solera", precio: 1500, imagen: "imagenes-proyecto/cervezapolar.jpg" }, // Corregí el nombre de la imagen
-    { id: 29, nombre: "Cerveza Polar", precio: 1800, imagen: "imagenes-proyecto/cervezapolar.jpg" } // Corregí el nombre de la imagen
-];
-/* Barra de navegacion*/
-let inputBuscador = document.getElementById('buscador')
-let botonBuscar = document.getElementById('buscar')
-botonBuscar.addEventListener('click', () => filtro (productoStock, inputBuscador, "nombre"))
-
-let botonFinalizarCompra = document.getElementById("finalizarCompra")
-  botonFinalizarCompra.addEventListener("click", finalizarCompra)
+    { id: 11, nombre: "Vino Tinto Alma Mora", precio: 3500, imagen: "imagenes-proyectovinotintoalmamora.jpg" },
+    { id: 12, nombre: "Vino Tinto Trapiche Cabernet Sauvignon", precio: 4000, imagen: "imagenes-proyecto/vinotintotrapiche.jpg" },
+    { id: 13, nombre: "Vino Blanco Trapiche Cosecha Tardía", precio: 3900, imagen: "imagenes-proyecto/vinoblancotrapiche.jpg" },
+    { id: 14, nombre: "Ron Santa Teresa", precio: 7000, imagen: "imagenes-proyecto/ronsantateresa.jpg" },
+    { id: 15, nombre: "Ron Cacique", precio: 6800, imagen: "imagenes-proyecto/roncacique.jpg" },
+    { id: 16, nombre: "Ron Carupano Añejo", precio: 7500, imagen: "imagenes-proyecto/roncarupano.jpg" },
+    { id: 17, nombre: "Cerveza Corona", precio: 1000, imagen: "imagenes-proyecto/cervezacorona.jpg" },
+    { id: 18, nombre: "Cerveza Solera", precio: 1500, imagen: "imagenes-proyecto/cervezapolar.jpg" },
+    { id: 29, nombre: "Cerveza Polar", precio: 1800, imagen: "imagenes-proyecto/cervezapolar.jpg" }
+  ];
+  /* Barra de navegacion*/
+  let inputBuscador = document.getElementById('buscador')
+  let botonBuscar = document.getElementById('buscar')
+  botonBuscar.addEventListener('click', () => filtrar(productoStock, inputBuscador, "nombre"))
 
   renderizarCarrito()
   renderizarTarjetas(productoStock)
 
 }
-/*carrito */
-function finalizarCompra() {
-  let carrito = recuperarCarrito()
-  if (carrito.length > 0) {
-    localStorage.removeItem("carrito")
-    renderizarCarrito()
-    alert("Muchas gracias por su compra, te esperamos pronto")
-  } else {
-    alert("Agrega productos en tu carrito para poder continuar")
-  }
+
+/* carrito */
+
+function filtrar(productos, input, propiedad) {
+  let valorInput = input.value.toLowerCase();
+  let productosFiltrados = productos.filter(producto => producto[propiedad].toLowerCase().includes(valorInput));
+  renderizarTarjetas(productosFiltrados);
 }
-function filtrar(productos, input,
-   propiedad) {
-  let productosFiltrados = productos.filter(producto => producto[propiedad])
-renderizarTarjetas(productosFiltrados)
-}
+
 function renderizarTarjetas(productos) {
-  let contenedor = document.getElementById("productos")
-  contenedor.innerHTML = ""
+  let contenedor = document.getElementById("productos");
+  contenedor.innerHTML = "";
   productos.forEach(producto => {
-    let tarjetaProducto = document.createElement("div")
-    tarjetaProducto.classList.add("tarjetaProducto")
+    let tarjetaProducto = document.createElement("div");
+    tarjetaProducto.classList.add("tarjetaProducto");
     tarjetaProducto.innerHTML = `
-      <h3>${producto.nombre}</h3>
-      <p>${producto.precio}</p>
-      <button id=${producto.id}>Agregar al carrito</button>
-    `
-    contenedor.appendChild(tarjetaProducto)
-    let botonAgregarAlCarrito = document.getElementById(producto.id)
-    botonAgregarAlCarrito.addEventListener("click", (e) => agregarAlCarrito(productos, e))
-    
-  })
-  }
-  function agregarAlCarrito(productos, e) {
-    let carrito = recuperarCarrito()
-    let productoBuscado = productos.find(producto => producto.id === Number(e.target.id))
-    let productoEnCarrito = carrito.find(producto => producto.id === productoBuscado.id)
+    <h3>${producto.nombre}</h3>
+    <p>${producto.precio}</p>
+    <button id=${producto.id}>Agregar al carrito</button>
+  `;
+    contenedor.appendChild(tarjetaProducto);
 
-localStorage.setItem("carrito", JSON.stringify(carrito))
-
-  renderizarCarrito()
+    let botonAgregarAlCarrito = document.getElementById(producto.id);
+    botonAgregarAlCarrito.addEventListener("click", () => agregarAlCarrito(producto));
+  });
 }
-function renderizarCarrito() {
-  let contenedor = document.getElementById("carrito")
-  contenedor.innerHTML = ""
-  let carrito = recuperarCarrito()
 
-  carrito.forEach(producto => {
-    let tarjetaProducto = document.createElement("div")
-    tarjetaProducto.innerHTML = `
-      <p>${producto.nombre}</p>
-      <p>${producto.precioUnitario}</p>
-      <p>${producto.unidades}</p>
-      <p>${producto.subtotal}</p>
-    `
-    contenedor.appendChild(tarjetaProducto)
-  })
+function renderizarCarrito() {
+  let contenedor = document.getElementById("carrito");
+  contenedor.innerHTML = "";
+  let carrito = recuperarCarrito();
+  let total = 0; 
+  if (carrito.length === 0) {
+    contenedor.innerHTML = "<p>El carrito está vacío</p>";
+  } else {
+    let tablaCarrito = document.createElement("table");
+    tablaCarrito.innerHTML = `
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>Precio</th>
+          <th>Cantidad</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `;
+
+    let tbody = tablaCarrito.querySelector("tbody");
+
+    carrito.forEach(producto => {
+      let fila = document.createElement("tr");
+      fila.innerHTML = `
+        <td>${producto.nombre}</td>
+        <td>${producto.precio}</td>
+        <td>${producto.cantidad}</td>
+        <td>${producto.precio * producto.cantidad}</td>
+      `;
+      tbody.appendChild(fila);
+
+      // Sumar el subtotal al total de la compra
+      total += producto.precio * producto.cantidad;
+    });
+
+    contenedor.appendChild(tablaCarrito);
+    let filaTotal = document.createElement("tr");
+    filaTotal.innerHTML = `
+      <td colspan="3">Total</td>
+      <td>${total}</td>
+    `;
+    tbody.appendChild(filaTotal);
+  }
+}
+
+function vaciarCarrito() {
+  localStorage.removeItem("carrito");
+let botonVaciarCarrito = document.getElementById("vaciarCarrito");
+botonVaciarCarrito.addEventListener("click", vaciarCarrito);
+botonVaciarCarrito.classList.add("boton-carrito");
+  renderizarCarrito();
+
+}
+
+console.log(localStorage.getItem("carrito"))
+
+function agregarAlCarrito(producto) {
+  let carrito = recuperarCarrito();
+  let productoEnCarrito = carrito.find(item => item.id === producto.id);
+
+  if (productoEnCarrito) {
+    productoEnCarrito.cantidad++;
+  } else {
+    carrito.push({ id: producto.id, nombre: producto.nombre, precio: producto.precio, cantidad: 1 });
+  }
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  renderizarCarrito();
 }
 
 function recuperarCarrito() {
-  return localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : []
-  }
-
+  return localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
+}
 /*footer*/
 
 document.getElementById('ir-arriba').addEventListener('click', function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 })
 
